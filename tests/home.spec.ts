5f2225ca881ff767c5176367ac62e743e892b791
Simone Taeggi <startae14@gmail.com>
2024-03-02 17:25:35 +0100

    Exercise: About page test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -8,4 +8,10 @@ test.describe('Home', () => {
     // verify title
     await expect(page).toHaveTitle('Practice E-Commerce Site – SDET Unicorns');
   })
+
+  test('Open About page and verifiy title', async ({ page }) => {
+    await page.goto('https://practice.sdetunicorns.com/about');
+
+    await expect(page).toHaveTitle('About – Practice E-Commerce Site');
+  })
 })
