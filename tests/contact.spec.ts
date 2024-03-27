7bba6365e0d3cd71e68180a87c77681695140174
Simone Taeggi <startae14@gmail.com>
2024-03-27 23:12:10 +0100

    Debugging: Playwright Inspector

--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -5,6 +5,8 @@ test.describe('Contact', () => {
     // open contact page
     await page.goto('https://practice.sdetunicorns.com/contact/');
 
+    await page.pause();
+
     // fill out the input fields
     await page.locator('.contact-name input').fill('Test Name');
     await page.locator('.contact-email input').fill('test@email.com');
