d6090a6aade0901f7f8b77360ceaa05dd2b9835b
Simone Taeggi <startae14@gmail.com>
2024-04-08 17:16:16 +0200

    exercise contact and Blog pages

--- tests/blog.spec.ts
+++ tests/blog.spec.ts
@@ -1,16 +1,19 @@
 import { test, expect } from '@playwright/test';
+import BlogPage from '../pages/blog.page';
 
 test.describe('Blog', () => {
+  let blogPage: BlogPage;
   test('Verify Recent Posts count and verify the length of each list item', async ({ page }) => {
+    blogPage = new BlogPage(page);
     // open blog page
-    await page.goto('https://practice.sdetunicorns.com/blog/');
+    await blogPage.navigate();
 
     // get the recent post list elements
-    const recentPostsList = page.locator('#recent-posts-3 ul li');
+    const recentPostsList = blogPage.recentPostsList;
 
     // loop through the list and assert the char length > 10
     for (const el of await recentPostsList.elementHandles()) {
-      const textContent = (await el.textContent())?.trim();
+      const textContent = (await el.textContent())!.trim();
       if (textContent) {
         expect(textContent.length).toBeGreaterThan(10);
       }
--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -1,28 +1,17 @@
 import { test, expect } from '@playwright/test';
+import ContactPage from '../pages/contact.page';
 
 test.describe('Contact', () => {
+  let contactPage: ContactPage;
   test('Fill contact form and verify success message', async ({ page }) => {
+    contactPage = new ContactPage(page);
     // open contact page
-    await page.goto('https://practice.sdetunicorns.com/contact/');
+    await contactPage.navigate();
 
-    await page.pause();
-
-    // fill out the input fields
-    await page.locator('.contact-name input').fill('Test Name');
-    await page.locator('.contact-email input').fill('test@email.com');
-    await page.locator('.contact-phone input').fill('13456743');
-    await page.locator('.contact-message textarea').fill('This is a test message');
-
-    // add a soft assertion
-    await expect(page.locator('.contact-message textarea')).toHaveText('Fail test message');
-
-    // click submit
-    await page.locator('button[type=submit]').click();
-
-    // Avoid running further if there were soft assertion failures.
-    // expect(test.info().errors).toHaveLength(0);
+    // fill out the input fields and submit
+    await contactPage.submitForm('Test Name', 'test@email.com', '13456743', 'This is a test message');
 
     // verify success message
-    await expect(page.locator('div[role=alert]')).toHaveText('Thanks for contacting us! We will be in touch with you shortly');
+    await expect(contactPage.successTxt).toHaveText('Thanks for contacting us! We will be in touch with you shortly');
   })
 })
