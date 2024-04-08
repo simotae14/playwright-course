e98d7e6d8016c4db84d85668df38eabed27cec72
Simone Taeggi <startae14@gmail.com>
2024-04-09 00:01:15 +0200

    Authentication with beforeAll

--- tests/account.spec.ts
+++ tests/account.spec.ts
@@ -1,7 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
-test.describe('My Account page', () => {
-  test.beforeEach(async ({ page }) => {
+test.describe.serial('My Account page', () => {
+  let page: Page;
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+
     await page.goto('/my-account');
     await page.locator('#username').fill('practiceuser1');
     await page.locator('#password').fill('PracticePass1!');
@@ -9,12 +12,12 @@ test.describe('My Account page', () => {
     await expect(page.locator('li a:has-text("Log out")')).toBeVisible();
   });
 
-  test('Access Orders', async ({ page }) => {
+  test('Access Orders', async () => {
     await page.locator(`li a[href*='orders']`).click();
     await expect(page).toHaveURL(/.*orders/);
   });
   
-  test('Access Downloads', async ({ page }) => {
+  test('Access Downloads', async () => {
     await page.locator(`li a[href*='downloads']`).click();
     await expect(page).toHaveURL(/.*downloads/);
   });
