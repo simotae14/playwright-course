475f3a088996bc92d990fd0c9096ada5d8e679e6
Simone Taeggi <startae14@gmail.com>
2024-04-08 12:33:34 +0200

    add HomePage Locators

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,7 +1,19 @@
 class HomePage {
   page: any;
+  getStartedButton: any;
+  headingText: any;
+  homeText: any;
+  searchIcon: any;
+  navLinks: any;
   constructor(page) {
     this.page = page;
+    this.getStartedButton = page.locator('#get-started');
+    this.headingText = page.getByText('Think different. Make different.', {
+      exact: true,
+    });
+    this.homeText = page.locator('#zak-primary-menu:has-text("Home")');
+    this.searchIcon = page.locator("//div[@class='zak-header-actions zak-header-actions--desktop']//a[@class='zak-header-search__toggle']");
+    this.navLinks = page.locator("#zak-primary-menu li[id*=menu]");
   }
 }
 
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,7 +4,6 @@ import HomePage from '../pages/home.page';
 test.describe('Home', () => {
   let homePage;
   test('Open HomePage and verifiy title', async ({ page }) => {
-    homePage = new HomePage(page);
     // open url
     await page.goto('https://practice.sdetunicorns.com/');
 
@@ -19,25 +18,25 @@ test.describe('Home', () => {
   })
 
   test('Click get started button using CSS Selector', async ({ page }) => {
+    homePage = new HomePage(page);
     await page.goto('https://practice.sdetunicorns.com');
 
     // verify url has not #get-started
     await expect(page).not.toHaveURL(/.*#get-started/);
 
     // Click the get started button.
-    await page.locator('#get-started').click();
+    await homePage.getStartedButton.click();
 
     // verify url has #get-started
     await expect(page).toHaveURL(/.*#get-started/);
   })
 
   test('Verify heading text is visible using text selector', async ({ page }) => {
+    homePage = new HomePage(page);
     await page.goto('https://practice.sdetunicorns.com');
 
     // find the text
-    const headingText = page.getByText('Think different. Make different.', {
-      exact: true,
-    });
+    const headingText = homePage.headingText;
 
     // verify heading text is visible
     await expect(headingText).not.toBeHidden();
@@ -45,26 +44,29 @@ test.describe('Home', () => {
   })
 
   test('Verify home link is enabled using text and css selector', async ({ page }) => {
+    homePage = new HomePage(page);
     await page.goto('https://practice.sdetunicorns.com');
 
     // find the home text
-    const homeText = page.locator('#zak-primary-menu:has-text("Home")');
+    const homeText = homePage.homeText;
 
     // verify heading text is enabled
     await expect(homeText).toBeEnabled();
   })
 
   test('Verify search icon is visible using xpath selector', async ({ page }) => {
+    homePage = new HomePage(page);
     await page.goto('https://practice.sdetunicorns.com');
 
     // find the search icon
-    const searchIcon = page.locator("//div[@class='zak-header-actions zak-header-actions--desktop']//a[@class='zak-header-search__toggle']");
+    const searchIcon = homePage.searchIcon;
 
     // verify search icon is visible
     await expect(searchIcon).toBeVisible();
   })
 
   test('Verify text of all nav links', async ({ page }) => {
+    homePage = new HomePage(page);
     const expectedLinks = [
       'Home',
       'About',
@@ -78,7 +80,7 @@ test.describe('Home', () => {
     await page.goto('https://practice.sdetunicorns.com');
 
     // find the nav links
-    const navLinks = page.locator("#zak-primary-menu li[id*=menu]");
+    const navLinks = homePage.navLinks;
 
     // print out all the links
     for (const el of await navLinks.elementHandles()) {
